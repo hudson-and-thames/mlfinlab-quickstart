aa6af0025952266267e5adc8a638c7d43a722c6e
Michael Struwig <michael.struwig@hudsonthames.org>
2023-08-29 14:02:45 +0200

    Update README.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# mlfinlab-quickstart
+# MLFinlab Quickstart
+
+This repository is a way to get up and running with `mlfinlab` quickly via Docker.
+
+Executing `python start.py` executes a `docker compose up` command that pulls
+down our `mlfinlab-base` image from DockerHub, injects your MLFinlab API key from
+your local environment, validates the key, and then remotely install MLFinlab,
+before finally launching a JupyterLab instance.
+
+All of this takes place and executes inside of the Docker container, isolating
+your system from any Python virtual environments or dependencies that you may
+have installed.
+
+## Requirements
+
+- Docker and docker-compose are installed.
+- Any version of Python
+
+## Instructions
+
+- Make sure your `MLFINLAB_API_KEY` environment variable is set.
+- Clone this repository
+- Execute `python start.py` from the within the project directory.
+- You are now up and running with MLFinlab!
--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 import subprocess
+import time
 import webbrowser
 
 # First make sure service isn't running
@@ -8,20 +9,20 @@ subprocess.check_call(cmd)
 # Start docker compose service
 cmd = ["docker", "compose", "up"]
 process = subprocess.Popen(
-    cmd,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.STDOUT,
-    text=True
+    cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
 )
+browser_opened = False
 
 try:
     for line in process.stdout:
-        print(line, end='')
-        if "127.0.0.1" in line:
+        print(line, end="")
+        if not browser_opened and "127.0.0.1" in line:
             url = line.split("|")[-1].strip()
             webbrowser.open(url)
-except KeyboardInterrupt:
+except KeyboardInterrupt:  # If the user interrupts the script...
+    # ...first kill the process
     process.terminate()
     process.wait()
+    # ...then kill the Docker service.
     cmd = ["docker", "compose", "down"]
     subprocess.check_call(cmd)
